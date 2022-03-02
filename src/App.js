61d67486e477791983616eb899635b28057b3258
ydh94 <dbeogud9411@gmail.com>
2022-03-03 02:03:39 +0900

    initial setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ReactPlayer url='https://www.youtube.com/watch?v=1-NK0HlOBV0' controls />
     </div>
   );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,7 @@
+import { useNavigate } from 'react-router-dom';
+
+function Nav() {
+  return <div></div>;
+}
+
+export default Nav;
