0777d1d50ba802d5d374470779640851847338c5
ydh94 <dbeogud9411@gmail.com>
2022-03-24 21:25:57 +0900

    Add - nav

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -1,21 +1,32 @@
-import { useEffect, useRef } from 'react';
-import ReactPlayer from 'react-player';
+import { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player/youtube';
 import '../../styles/projects/Project.scss';
 
 interface IPropsProject {
   data: { video: string; title: string; content: string };
   key: number;
+  focused: boolean;
 }
 
 export default function Project(attr: IPropsProject) {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const videoRef = useRef<ReactPlayer>(null);
   const { video, title, content } = attr.data;
+  const { focused } = attr;
+  const [play, setPlay] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (focused) {
+      setTimeout(() => {
+        setPlay(true);
+      }, 1000);
+    } else {
+      setPlay(false);
+    }
+  }, [focused]);
 
   return (
-    <div className='project--container' ref={containerRef}>
+    <div className='project--container'>
       <div className='project--video'>
-        <ReactPlayer url={video} controls width={'100%'} height={'100%'} ref={videoRef} playing={true} />
+        <ReactPlayer url={video} controls width={'100%'} height={'100%'} playing={play} />
       </div>
       <div className='project--article'>
         <div className='project--title'>{title}</div>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,88 +1,58 @@
 import Nav from 'components/Nav';
 import Project from 'components/projects/Project';
 import data from 'data';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import '../styles/Projects.scss';
 
 export default function Projects() {
   const containerRef = useRef<HTMLDivElement>(null);
+  const [focusPage, setFocusPage] = useState(0);
 
   useEffect(() => {
+    let page = 0;
+
+    const { current } = containerRef;
+    const scrollHandler = (page: number) => {
+      const pageHeight = window.innerHeight;
+      current?.scrollTo({
+        top: (pageHeight - 77.5) * page,
+        left: 0,
+        behavior: 'smooth',
+      });
+    };
+
     const wheelHandler = (e: WheelEvent) => {
       e.preventDefault();
       const { deltaY } = e;
-      console.log(containerRef.current?.scrollTop);
-      const { scrollTop } = containerRef.current;
-      const pageHeight = window.innerHeight;
 
       if (deltaY > 0) {
-        // 스크롤 내릴 때
-        if (scrollTop >= 0 && scrollTop < pageHeight) {
-          //현재 1페이지
-          current?.scrollTo({
-            top: pageHeight,
-            left: 0,
-            behavior: 'smooth',
-          });
-        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
-          //현재 2페이지
-
-          current?.scrollTo({
-            top: pageHeight * 2,
-            left: 0,
-            behavior: 'smooth',
-          });
-        } else {
-          // 현재 3페이지
-
-          current?.scrollTo({
-            top: pageHeight * 2,
-            left: 0,
-            behavior: 'smooth',
-          });
+        if (page < data.length - 1) {
+          page++;
+          setFocusPage(page);
+          scrollHandler(page);
         }
       } else {
-        // 스크롤 올릴 때
-        if (scrollTop >= 0 && scrollTop < pageHeight) {
-          //현재 1페이지
-          current?.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth',
-          });
-        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
-          //현재 2페이지
-
-          current?.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth',
-          });
-        } else {
-          // 현재 3페이지
-
-          current?.scrollTo({
-            top: pageHeight,
-            left: 0,
-            behavior: 'smooth',
-          });
+        if (page > 0) {
+          page--;
+          setFocusPage(page);
+          scrollHandler(page);
         }
       }
     };
 
-    const { current } = containerRef;
-    current?.addEventListener('wheel', wheelHandler);
-    return () => {
-      current?.removeEventListener('wheel', wheelHandler);
+    document.onkeydown = function (e: globalThis.KeyboardEvent) {
+      e.preventDefault();
     };
+
+    current?.addEventListener('wheel', wheelHandler);
   }, []);
 
   return (
     <div className='projects--container' ref={containerRef}>
       <Nav />
       {data.map((e, i) => {
-        return <Project key={i} data={e} />;
+        return <Project key={i} data={e} focused={focusPage === i} />;
       })}
     </div>
   );
