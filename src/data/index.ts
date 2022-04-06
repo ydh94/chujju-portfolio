cb4a02de2b23ede000c4122ce3c3d1b3df65808d
ydh94 <dbeogud9411@gmail.com>
2022-04-07 03:57:41 +0900

    update data

--- src/data/index.ts
+++ src/data/index.ts
@@ -22,6 +22,45 @@ const data = [
     title: 'BTS',
     content: '노블 공기청정기',
   },
+  {
+    video: 'https://www.youtube.com/watch?v=1-NK0HlOBV0',
+    title: '페이커',
+    content: '월드콘',
+    timeline: [
+      {
+        img: 'https://t1.daumcdn.net/cfile/tistory/2341B34A58070D4E04',
+        sec: 3,
+      },
+      { img: 'https://t1.daumcdn.net/cfile/tistory/2341B34A58070D4E04', sec: 5 },
+      { img: 'https://t1.daumcdn.net/cfile/tistory/2341B34A58070D4E04', sec: 8 },
+    ],
+  },
+  {
+    video: '',
+    title: 'XX한번잘못샀다가 골로간 현실부부.avi',
+    content: '월드콘',
+    timeline: [
+      {
+        img: 'https://t1.daumcdn.net/cfile/tistory/2341B34A58070D4E04',
+        sec: 3,
+      },
+      { img: 'https://t1.daumcdn.net/cfile/tistory/2341B34A58070D4E04', sec: 5 },
+      { img: 'https://t1.daumcdn.net/cfile/tistory/2341B34A58070D4E04', sec: 8 },
+    ],
+  },
+  {
+    video: 'https://www.youtube.com/watch?v=0-ZDzzYm1MY',
+    title: '꿈을 향해 ᄀᄀ, 꿈을향해금강',
+    content: '금강대학교',
+    timeline: [
+      {
+        img: 'https://t1.daumcdn.net/cfile/tistory/2341B34A58070D4E04',
+        sec: 3,
+      },
+      { img: 'https://t1.daumcdn.net/cfile/tistory/2341B34A58070D4E04', sec: 5 },
+      { img: 'https://t1.daumcdn.net/cfile/tistory/2341B34A58070D4E04', sec: 8 },
+    ],
+  },
 ];
 
 export default data;
